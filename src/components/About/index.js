1aeffcdd068986cc2ff63252abd92df12cfb556b
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-09-30 12:16:39 -0700

    About component and initial Nav component

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import coverImage from '../../assets/images/bg_microchip.jpg';
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About me</h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: '100%' }}
+        alt="cover"
+      />
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <header>
+      <div className="flex-row">
+        <h1>
+          <a href="/">Sandrine Poissonnet</a>
+        </h1>
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a href="#about">About me</a>
+            </li>
+            <li className="mx-2">
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li className="mx-2">
+              <a href="#contact">Contact</a>
+            </li>
+            <li className="mx-2">
+              <a href="#resume">Resume</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Nav;
