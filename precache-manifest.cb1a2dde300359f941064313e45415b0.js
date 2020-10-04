0964ea2a72390a2e424089524991a29e1bddf649
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-04 14:51:04 -0700

    Updates

---
+++ precache-manifest.cb1a2dde300359f941064313e45415b0.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73340b62d1ae29a06c9dadf2f94c3fcb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "969f2082e038e8c57280",
+    "url": "/react-portfolio/static/css/main.ac376408.chunk.css"
+  },
+  {
+    "revision": "8c2e49719a47fe444a49",
+    "url": "/react-portfolio/static/js/2.12b1d744.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.12b1d744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969f2082e038e8c57280",
+    "url": "/react-portfolio/static/js/main.1c3d1178.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "ae67fc45a991d51ec967fc871670c67f",
+    "url": "/react-portfolio/static/media/bg_microchip.ae67fc45.jpg"
+  },
+  {
+    "revision": "6fdc1d8cc44c889e4ca4c15a22f88639",
+    "url": "/react-portfolio/static/media/logo-dintel 2.6fdc1d8c.png"
+  },
+  {
+    "revision": "6fdc1d8cc44c889e4ca4c15a22f88639",
+    "url": "/react-portfolio/static/media/logo-dintel.6fdc1d8c.png"
+  },
+  {
+    "revision": "e05214c577ea20489e8cb4fc1492c7a7",
+    "url": "/react-portfolio/static/media/nasa 2.e05214c5.jpg"
+  },
+  {
+    "revision": "e05214c577ea20489e8cb4fc1492c7a7",
+    "url": "/react-portfolio/static/media/nasa.e05214c5.jpg"
+  },
+  {
+    "revision": "a81a53c81740465d322f68499c1e0472",
+    "url": "/react-portfolio/static/media/pic_sandrine.a81a53c8.jpg"
+  },
+  {
+    "revision": "5510a2c5eadec0c960d20c4ce5c582f4",
+    "url": "/react-portfolio/static/media/resume.5510a2c5.pdf"
+  },
+  {
+    "revision": "a56a3be1e48286890be245063b85f17e",
+    "url": "/react-portfolio/static/media/screen-shot-belly-button 2.a56a3be1.png"
+  },
+  {
+    "revision": "a56a3be1e48286890be245063b85f17e",
+    "url": "/react-portfolio/static/media/screen-shot-belly-button.a56a3be1.png"
+  },
+  {
+    "revision": "ffa52dace252d8d07d6192fc10fb504c",
+    "url": "/react-portfolio/static/media/screen-shot-coviesearch 2.ffa52dac.png"
+  },
+  {
+    "revision": "ffa52dace252d8d07d6192fc10fb504c",
+    "url": "/react-portfolio/static/media/screen-shot-coviesearch.ffa52dac.png"
+  },
+  {
+    "revision": "31934e8b950cdbd67e5eeb1ad9990918",
+    "url": "/react-portfolio/static/media/screen-shot-d3times 2.31934e8b.png"
+  },
+  {
+    "revision": "31934e8b950cdbd67e5eeb1ad9990918",
+    "url": "/react-portfolio/static/media/screen-shot-d3times.31934e8b.png"
+  },
+  {
+    "revision": "0731c22613512ddfff8ad3bb48aef52f",
+    "url": "/react-portfolio/static/media/screen-shot-run-buddy 2.0731c226.png"
+  },
+  {
+    "revision": "0731c22613512ddfff8ad3bb48aef52f",
+    "url": "/react-portfolio/static/media/screen-shot-run-buddy.0731c226.png"
+  },
+  {
+    "revision": "b6f52494d42abc7decdeed25ab69f8d0",
+    "url": "/react-portfolio/static/media/screen-shot-usgs 2.b6f52494.png"
+  },
+  {
+    "revision": "b6f52494d42abc7decdeed25ab69f8d0",
+    "url": "/react-portfolio/static/media/screen-shot-usgs.b6f52494.png"
+  },
+  {
+    "revision": "cc4b137d6b6205de5f44d79a89dfbe85",
+    "url": "/react-portfolio/static/media/screen-shot-weather-dashboard 2.cc4b137d.png"
+  },
+  {
+    "revision": "cc4b137d6b6205de5f44d79a89dfbe85",
+    "url": "/react-portfolio/static/media/screen-shot-weather-dashboard.cc4b137d.png"
+  },
+  {
+    "revision": "d986b8f8129c68820be9d2b494015cd5",
+    "url": "/react-portfolio/static/media/screen-shot-whr 2.d986b8f8.png"
+  },
+  {
+    "revision": "d986b8f8129c68820be9d2b494015cd5",
+    "url": "/react-portfolio/static/media/screen-shot-whr.d986b8f8.png"
+  },
+  {
+    "revision": "fd12614064169f65c30013d7926df37a",
+    "url": "/react-portfolio/static/media/shrinko 2.fd126140.jpg"
+  },
+  {
+    "revision": "fd12614064169f65c30013d7926df37a",
+    "url": "/react-portfolio/static/media/shrinko.fd126140.jpg"
+  },
+  {
+    "revision": "e9b1a76056a5ed8b7c2477d4386e1afc",
+    "url": "/react-portfolio/static/media/the-tech-blog 2.e9b1a760.jpg"
+  },
+  {
+    "revision": "e9b1a76056a5ed8b7c2477d4386e1afc",
+    "url": "/react-portfolio/static/media/the-tech-blog.e9b1a760.jpg"
+  }
+]);
